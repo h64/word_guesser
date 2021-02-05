e09e06dc9f67a7bd88105fa095539d6521f3f234
sunil lama <72329793+SunilDeveloper7@users.noreply.github.com>
2021-02-05 10:02:37 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 /* Constants */
-const WORD_LIST = ['producer', 'brainstorm', 'explosion', 'soup', 'feather']
+const WORD_LIST = ['warriors', 'atmosphere', 'thrown', 'general', 'assembly']
 
 /* Variables and App State */
 let word = "";
+let wordStatus = [];
 
 /* DOM References */
 let wordContainer = document.querySelector('#guess-word-container');
@@ -16,6 +17,8 @@ let messages = document.querySelector('#messages');
 // 2. Display the word blanks in the DOM
 const initialize = event => {
     word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)]
+    wordStatus = word.split('').map(l => '_');
+
     console.log('The word is:', word);
     displayWordStatus();
 }
@@ -28,7 +31,7 @@ const displayWordStatus = () => {
     }
     for(let i = 0; i < word.length; i++) {
         let letter = document.createElement('div');
-        letter.textContent = '_'
+        letter.textContent = wordStatus[i];
         letter.classList.add("letter");
         wordContainer.appendChild(letter);
     }
@@ -37,12 +40,48 @@ const displayWordStatus = () => {
 // On submit event: Guess a letter or guess the whole word
 const guessLetter = event => {
     event.preventDefault();
-    console.log(`You submitted: ${textBox.value}`);
+    let guess = textBox.value;
+    if(guess.length == 0) return; // Reject empty submission
+
+    letters = word.split('');
+    // console.log(`You submitted: ${guess}`);
+    // console.log('Letters:', letters);
+
+    if(guess.length == 1) { // Guessing one letter
+        if(letters.includes(guess)) {
+            displayMessage(`${guess} is a match!`)
+            for(let i = 0; i < word.length; i++) {
+                if(word[i] == guess) {
+                    wordStatus[i] = guess;
+                } 
+            }
+            displayWordStatus();
+            if(!wordStatus.includes("_")) {
+                displayMessage("Congrats! You've guessed the whole word!")
+            }
+            
+        } else {
+            displayMessage(`${guess} is not a match.`)
+        }
+    } else {
+        if(guess == word) {
+            displayMessage("Congrats! You've guessed the whole word!")
+            wordStatus = letters;
+            displayWordStatus();
+        } else {
+            displayMessage(`Sorry! ${guess} is not the word I'm thinking of!`);
+        }
+    }
 }
 
-// Display a message to the user in the messagebox
-const displayMessage = msg => { 
-    /* Your code here! */
+const displayMessage = msg => {
+    // Clear all elements inside messages
+    while(messages.firstChild) {
+        messages.removeChild(messages.firstChild);
+    }
+    let p = document.createElement('p');
+    p.textContent = msg;
+    messages.appendChild(p);
 }
 
 /* Event Listeners */
