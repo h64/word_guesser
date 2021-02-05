3541b89290d6613b8c03bf67e2ccf85f34a61470
wonkatin <j.drawers@gmail.com>
2021-02-04 22:06:47 -0600

    Initial Commit: Working on checking the value of input against secret word

--- README.md
+++ README.md
@@ -8,26 +8,29 @@ Fortunately, the starter code already has a substantial amount of coding done fo
 
 ## Functionality
 
-* Store a list (or tuple) of 5 to 10 words in your script.
+<!-- * Store a list (or tuple) of 5 to 10 words in your script.
 * Randomly choose a word from this list as the secret word.
-* Display the unrevealed word as underscores (with the same length.)
-* Prompt the user to enter a letter.
-* If the letter is in the word, mark it as revealed and visually display that letter in the word.
-* If the letter is incorrect, indicate to the user that the guess was incorrect.
+* Display the unrevealed word as underscores (with the same length.) -->
+
+<!-- - Prompt the user to enter a letter. -->
+
+- If the letter is in the word, mark it as revealed and visually display that letter in the word.
+- If the letter is incorrect, indicate to the user that the guess was incorrect.
 
 ### Hints
 
-* Your game will need some state to keep track of the word, how many letters are yet to be guessed, and the current state of the hangman.
-* You can initially represent the hangman as a decreasing number of guesses remaining.
-* You can use the `in` keyword or `.includes()` to test to see if a letter is in the secret word.
-* Remember to account for case differences.
+- Your game will need some state to keep track of the word, how many letters are yet to be guessed, and the current state of the hangman.
+- You can initially represent the hangman as a decreasing number of guesses remaining.
+- You can use the `in` keyword or `.includes()` to test to see if a letter is in the secret word.
+- Remember to account for case differences.
 
 ### Pseudocode
 
-1. Initialize the game: Initialize all variables to default values.
+<!-- 1. Initialize the game: Initialize all variables to default values.
 2. Randomly select a secret word.
 3. Display the word as blanks.
-4. Ask the user for a letter.
+4. Ask the user for a letter. -->
+
 5. Determine if letter is correct or incorrect.
 6. If incorrect, tell the user it's incorrect.
 7. If correct, add the letter to the guessed list, redraw the secret word with the new letter(s) showing.
@@ -43,12 +46,15 @@ Being able to render a word as some underscores and some letters will be a littl
 4. To decide if you should render a blank underscore or render a correct letter, you can iterate over the secret word checking to see if each letter is in the guessed letter list. If it is, use the actual letter character. If it is not, use an underscore to represent a letter not yet guessed.
 
 ## No Match
+
 ![screenshot no match](assets/no-match.png)
+
 ## Match
+
 ![screenshot match](assets/match.png)
+
 ## BONUS
 
 Sometimes a user might have solved the answer without guessing every single letter. As a bonus implement a way for the user to guess the whole phrase instead of just one letter. Preferably, this should be case insensitive!
-
 
 ![screenshot finished](assets/finish.png)
--- js/app.js
+++ js/app.js
@@ -1,50 +1,103 @@
 /* Constants */
-const WORD_LIST = ['producer', 'brainstorm', 'explosion', 'soup', 'feather']
+const WORD_LIST = ["producer", "brainstorm", "explosion", "soup", "feather"];
 
 /* Variables and App State */
 let word = "";
 
 /* DOM References */
-let wordContainer = document.querySelector('#guess-word-container');
-let textBox = document.querySelector('#textbox');
-let messages = document.querySelector('#messages');
+let wordContainer = document.querySelector("#guess-word-container");
+let textBox = document.querySelector("#textbox");
+let messages = document.querySelector("#messages-container");
 
 /* Functions and app logic */
 
-// Initialize the game: 
+// Initialize the game:
 // 1. Reset state variables
 // 2. Display the word blanks in the DOM
-const initialize = event => {
-    word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)]
-    console.log('The word is:', word);
-    displayWordStatus();
-}
+const initialize = (event) => {
+  word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)];
+  console.log("The word is:", word);
+  displayWordStatus();
+};
 
 // Helper function that adds multiple <div>_</div> to DOM
 const displayWordStatus = () => {
-    // Clear(empty) all of the divs children 
-    while(wordContainer.firstChild) {
-        wordContainer.removeChild(wordContainer.firstChild);
-    }
-    for(let i = 0; i < word.length; i++) {
-        let letter = document.createElement('div');
-        letter.textContent = '_'
-        letter.classList.add("letter");
-        wordContainer.appendChild(letter);
-    }
-}
+  // Clear(empty) all of the divs children
+  while (wordContainer.firstChild) {
+    wordContainer.removeChild(wordContainer.firstChild);
+  }
+  for (let i = 0; i < word.length; i++) {
+    let letter = document.createElement("div");
+    letter.textContent = "_";
+    letter.classList.add("letter");
+    wordContainer.appendChild(letter);
+  }
+};
+const appendMessage = (message) => {
+  messages.appendChild(messages(message));
+};
 
 // On submit event: Guess a letter or guess the whole word
-const guessLetter = event => {
-    event.preventDefault();
-    console.log(`You submitted: ${textBox.value}`);
-}
+const guessLetter = (event) => {
+  event.preventDefault();
+
+  let input = textbox.value;
+  if (input.length > 1) {
+    console.log("please pick only one letter");
+  }
+  if (input.length == 1) {
+    console.log(input);
+  } else {
+    console.log("please submit a letter");
+  }
+
+  letters = word.split("");
+  console.log(letters);
+  if (letters.includes(input)) {
+    console.log(input);
+  } else console.log("pick another letter");
+  console.log(`You submitted: ${textBox.value}`);
+};
 
 // Display a message to the user in the messagebox
-const displayMessage = msg => { 
-    /* Your code here! */
-}
+const displayMessage = (msg) => {
+  /* Your code here! */
+};
 
 /* Event Listeners */
-document.addEventListener('DOMContentLoaded', initialize);
-document.addEventListener('submit', guessLetter);
+document.addEventListener("DOMContentLoaded", initialize);
+document.addEventListener("submit", guessLetter);
+
+// CODE-ALONG
+// /* Constants */
+// const WORD_LIST = ["producer", "brainstorm", "explosion", "soup", "feather"];
+// /* Game Logic Variables and State */
+// let secretWord = "";
+// let unguessedWord = [];
+// /* DOM References */
+// let guessForm = document.querySelector("#guess-form");
+// let messageContainer = document.querySelector("#message-container");
+// let wordContainer = document.querySelector("#guess-word-container");
+// /* Functions and Game Logic */
+// const initialize = (event) => {
+//   secretWord = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)];
+//   unguessedWord = secretWord.split("").map((letter) => "_");
+//   console.log(unguessedWord);
+//   console.log(secretWord);
+//   displayWordStatus();
+// };
+// const handleSubmit = (event) => {};
+// displayWordStatus = () => {
+//   while (wordContainer.firstChild) {
+//     wordContainer.removeChild(wordContainer.firstChild);
+//   }
+//   unguessedWord.forEach((letter) => {
+//     let letterDiv = document.createElement("div");
+//     letterDiv.textContent = letter;
+//     letterDiv.classList.add("letter");
+//     wordContainer.appendChild(letterDiv);
+//   });
+// };
+// /* Event Listeners */
+// document.addEventListener("DOMContentLoaded", initialize);
+// guessForm.addEventListener("submit", handleSubmit);
