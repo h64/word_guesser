eb39bef5c08946d69596345ceb6cca92d80f8c14
darrengp <ceo.darrenperry@gmail.com>
2021-02-05 07:43:28 -0800

    late save

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/* Constants */
+const WORD_LIST = ["kiwi", "strawberry", "apple", "pear"];
+
+const guesses = document.getElementById("guesses");
+const guessForm = document.getElementById("guess-form");
+/* Game Logic Variables and State */
+let word = "";
+let unguessedWord = [];
+let textBox = document.getElementById("textbox");
+/* DOM References */
+
+let guesForm = document.getElementById("guess-form");
+let messageContainer = document.getElementById("message-container");
+let wordContainer = document.getElementById("word-container");
+
+/* Functions and Game Logic */
+const initialize = (event) => {
+  word = WORD_LIST[Math.floor(Math.random() * WORD_LIST.length)];
+  console.log(word);
+  unguessedWord = word.split(" ").map((letter) => "_");
+  displayWordStatus();
+};
+
+const handleSubmit = (event) => {};
+
+const displayWordStatus = () => {
+  // Clear(empty) all of the divs children
+  while (wordContainer.firstChild) {
+    wordContainer.removeChild(wordContainer.firstChild);
+  }
+  for (let i = 0; i < word.length; i++) {
+    let letter = document.createElement("div");
+    letter.textContent = "_";
+    letter.classList.add("letter");
+    wordContainer.appendChild(letter);
+  }
+};
+
+// add to DOM all of the unguessedWord letters and _ 's
+
+unguessedWord.forEach((letter) => {
+  let letterDiv = document.createElement("div");
+  letterDiv.textContent = letter;
+  letterDiv.classList;
+  wordContainer.appendChild(letterDiv);
+});
+// On submit event: Guess a letter or guess the whole word
+const guessLetter = (event) => {
+  event.preventDefault();
+  console.log(`You submitted: ${textBox.value}`);
+  for (let n in word) {
+    if (textBox.value == word[n]) {
+      console.log(word[n]);
+    }
+  }
+};
+/* Event Listeners */
+document.addEventListener("DOMContentLoaded", initialize);
+guessForm.addEventListener("submit", guessLetter);
